746305f761a3b54b22c81e28c858cc4a7033b6bd
ACER <mhdiqbaltan@gmail.com>
2020-04-07 00:43:57 +0700

    fix bug tombol reset gak berfungsi :v

--- js/game.js
+++ js/game.js
@@ -1,43 +1,60 @@
+// ngimport kata2 yang mau dirandom dari namaHewan.js
 import { randomWord } from './namaHewan.js';
 
+// ngatur variable bosquuh
 var answer = document.getElementById('answer');
 var word = document.getElementById('word');
+var tombolReset = document.getElementById('reset');
 var answered = [];
-
-let pickedWord = randomWord();
-
-for (let x in pickedWord) {
-	answered.push('_');
-}
-
-answer.innerHTML = answered.join(' ');
-
-let guessedWord;
-// do while loop, untuk ngecek apakah kata yang dishuffle masih sama dengan kata asli nya
-// jika masih sama maka kata tersebut akan di shuffle lagi
-do {
-	guessedWord = shuffle(pickedWord.split('')).join('');
-} while (pickedWord == guessedWord);
-
-for (let i in guessedWord) {
-	word.innerHTML += `<span class='huruf'>${guessedWord[i]}</span>`;
-}
-
-var hurufs = document.querySelectorAll('.huruf');
-
-for (let i = 0; i < hurufs.length; i++) {
-	hurufs[i].onclick = function() {
-		handleGuess(this.textContent);
-		this.style.display = 'none';
-	};
-}
-
-// guessedWord.split('').map((huruf, i) => {
-// 	document.getElementById(i).addEventListener('click', () => handleGuess(huruf));
-// });
-
 let count = 0;
-function handleGuess(huruf) {
+let pickedWord = randomWord();
+let guessedWord;
+
+tombolReset.onclick = function() {
+	reset();
+};
+
+reset();
+
+function reset() {
+	count = 0;
+	answered = [];
+	pickedWord = randomWord();
+	word.innerHTML = '';
+	for (let x in pickedWord) {
+		answered.push('_');
+	}
+	answer.innerHTML = answered.join(' ');
+	// do while loop, untuk ngecek apakah kata yang dishuffle masih sama dengan kata asli nya
+	// jika masih sama maka kata tersebut akan di shuffle lagi
+	do {
+		guessedWord = shuffle(pickedWord.split('')).join('');
+	} while (pickedWord == guessedWord);
+
+	//ngeloop untuk nampilkan huruf yang diacak tadi
+	for (let i in guessedWord) {
+		// tiap huruf ada <span> nya, inspect aja kalo gak caya
+		// terus kutambah class huruf biar bisa di queryselector
+		word.innerHTML += `<span class='huruf'>${guessedWord[i]}</span>`;
+	}
+
+	// siap ditampilkan huruf yang mau ditebak,
+	// aku buat variabl hurufs, yaitu semua yang ada class .huruf
+	var hurufs = document.querySelectorAll('.huruf');
+
+	// kan hurufs itu jamak, artinya banyak, jadi ku loop lah
+	// pake for loop aja, soalnya cuman mau makek i nya buat index
+	for (let i = 0; i < hurufs.length; i++) {
+		// tiap huruf di hurufs kalo diklik maka pindah dia ke kolom answer/jawaban
+		hurufs[i].onclick = function() {
+			isiJawaban(this.textContent); //nambahkan huruf ke answer
+			this.style.display = 'none'; //ngehapus display huruf
+		};
+	}
+}
+
+// fungsi untuk ngisi kolom answer/jawaban
+function isiJawaban(huruf) {
 	answered[count] = huruf;
 	count++;
 	answer.innerHTML = answered.join(' ');
