1a55cc3e6717463504fa3382e26c1948898e9715
unknown <rizki.pojan@gmail.com>
2020-04-07 11:04:20 +0700

    nambah nama hewan :v

--- js/namaHewan.js
+++ js/namaHewan.js
@@ -1,4 +1,5 @@
-var hewan = [ 'ikan', 'ayam', 'bebek', 'burung', 'sapi', 'beruang', 'harimau', 'kancil', 'rusa', 'anjing', 'kucing' ];
+var hewan = [ 'ikan', 'ayam', 'bebek', 'burung', 'sapi', 'beruang', 'harimau', 'kancil', 'rusa', 'anjing', 'kucing', 'bison', 'badak',
+ 'singa', 'jerapah', 'paus', 'zebra', 'kudanil', 'kuda' ];
 
 function randomWord() {
 	return hewan[Math.floor(Math.random() * hewan.length)];
