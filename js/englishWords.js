8bfbb6f2e86e36fcc07797baac70476b8da7e9fd
radiffafadhil <radiffalubis@gmail.com>
2020-06-09 10:18:55 +0700

    nambah kata

--- js/englishWords.js
+++ js/englishWords.js
@@ -1,4 +1,6 @@
-var words = [ 'dog', 'bird', 'car', 'cow', 'bear', 'spoon', 'tiger', 'water', 'father', 'son', 'sun' ];
+var words = [ 'dog', 'bird', 'car', 'cow', 'bear', 'spoon', 'tiger', 'water', 'father', 'son', 'sun',
+'sky', 'mother', 'computer', 'television', 'house', 'headphone', 'headset', 'handphone', 'smartphone',
+'phone', 'monitor', 'aquarium', 'window', 'school' ];
 
 function randomWord() {
 	return words[Math.floor(Math.random() * words.length)];
